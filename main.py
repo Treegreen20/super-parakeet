6daede6c9b112f067583608b6256e7a09140e7a0
Owen Singer <46035591+ Owensinger@users.noreply.github.com>
2019-06-13 12:25:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ import turtle
 win = turtle.Screen()
 turtle = turtle.Turtle()
 
-for i in range (4):
-    turtle.right(90)
-    turtle.forward(70)
+def right():
+    turtle.right(45)
+
+def left ():
+    turtle.left(45)
+
+def forward():
+    turtle.ft(100)
+
+
+win.listen()
+
+win.onkey()
+win.onkey()
+win.onkey()
+win.onkey()
+win.onkey()
+
 
 
 win.mainloop()
-
